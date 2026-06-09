a48bd66135ca0eda167184ddb527de3ff1180a52
Isabella Stephens <imstephens.professional@gmail.com>
2026-06-09 12:04:50 -0400

    Update start_branches.py

--- Bank/start_branches.py
+++ Bank/start_branches.py
@@ -1,3 +1,7 @@
+# Author Names: Isabella Stephens, Sosan Wahid, Andrew Kivrak
+# Created: November 2022
+# Updated: November 2022
+
 from subprocess import Popen, PIPE
 from multiprocessing import Process
 import multiprocessing
